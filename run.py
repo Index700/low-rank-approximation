a5fed3e1362137967fb1eecb326cb54fdf34c311
yukiue <45696312+yukiue@users.noreply.github.com>
2020-06-26 21:01:17 +0900

    update

--- run.py
+++ run.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python3
+#
+# usage: python3 run.py Airport.bmp
 
 from PIL import Image
 import numpy as np
+import os
+import sys
 
-image = np.array(Image.open('Man.bmp'))
+filename = sys.argv[1]
+basename = os.path.splitext(os.path.basename(filename))[0]
 
-# print(image.shape)
-# (1024, 1024)
+X = np.array(Image.open(filename))
 
 # u: left-singular vector
 # s: singular value
 # v: right-singular vector
-u, s, v = np.linalg.svd(image)
+u, s, v = np.linalg.svd(X)
 
-k = 20
-ur = u[:, :k - 1]
-sr = np.diag(s[:k - 1])
-vr = v[:k - 1, :]
-Mk = np.dot(np.dot(ur, sr), vr)
+os.mkdir('images')
 
-pil_image = Image.fromarray(Mk)
-pil_image = pil_image.convert('RGB')
-pil_image.save('image.jpg')
+for k in [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]:
+    ur = u[:, :k - 1]
+    sr = np.diag(s[:k - 1])
+    vr = v[:k - 1, :]
+    Xk = np.dot(np.dot(ur, sr), vr)
+
+    image = Image.fromarray(Xk)
+    image = image.convert('RGB')
+    image.save(f'images/{basename}-{k}.jpg')
